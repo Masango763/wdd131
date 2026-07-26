1dc5f40cf7ae3559ffd181d4c88c599aa40c2f9a
Masango Dewheretsoko <masangodewhe@gmail.com>
2026-07-26 16:51:22 +0200

    Add getdates.js for dynamic footer year and last modified date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
-// Dynamically set the current year for the copyright notice
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+// Set current year in copyright
+const currentYearSpan = document.getElementById("currentyear");
+if (currentYearSpan) {
+    currentYearSpan.textContent = new Date().getFullYear();
+}
 
-// Dynamically set the last modified date of the document
-document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
-=======
-const yearSpan = document.getElementById("currentyear");
-const lastModified = document.getElementById("lastModified");
-
-yearSpan.textContent = new Date().getFullYear();
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
->>>>>>> d9bfdc3d2108aa113295e80c3a21d5eb91fe3fd1
+// Set last modified date in footer
+const lastModifiedParagraph = document.getElementById("lastModified");
+if (lastModifiedParagraph) {
+    lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
+}
